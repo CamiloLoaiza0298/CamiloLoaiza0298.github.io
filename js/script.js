0924ba4d52503f942953f849f3cc37a41e507311
CamiloLoaiza0298 <ingjcloaiza@gmail.com>
2025-08-05 22:55:04 -0400

    big progress

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
 }
 
 let animated = document.querySelectorAll(".animated");
+
+document.querySelector('.contact-form').addEventListener('submit', function (e) {
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const subject = document.getElementById('subject').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (!name || !email || !subject || !message) {
+      e.preventDefault();
+      alert('Please fill in all the fields before submitting.');
+    }
+  });
